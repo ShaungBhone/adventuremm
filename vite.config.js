039f023370e433a49caee5b7c6f8c8cd127064d7
Hein <heinminthant1999@gmail.com>
2023-05-25 22:22:52 +0630

    test

--- vite.config.js
+++ vite.config.js
@@ -2,6 +2,13 @@ import { defineConfig } from "vite";
 import laravel from "laravel-vite-plugin";
 
 export default defineConfig({
+    server: {
+        https: true,
+        host: "0.0.0.0",
+        hmr: {
+            host: "localhost",
+        },
+    },
     plugins: [
         laravel({
             input: [
