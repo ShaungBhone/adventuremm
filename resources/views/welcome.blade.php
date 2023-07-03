15c4512c6f80a2325ec79d9f06e62ab49abf7227
ShaungBhone <phonelaayy@gmail.com>
2023-07-03 15:08:18 +0630

    some changes for blog

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -55,8 +55,8 @@
             <!-- Small Features Item -->
             <div class="animate_top kn to/3 tc cg oq">
               <div class="tc wf xf cf ae cd rg mh">
-                <svg xmlns="http://www.w3.org/2000/svg"
-                  viewBox="0 0 320 512" class="h-10 w-10 fill-white">
+                <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 320 512"
+                  class="h-10 w-10 fill-white">
                   <path
                     d="M160 0c17.7 0 32 14.3 32 32V67.7c1.6 .2 3.1 .4 4.7 .7c.4 .1 .7 .1 1.1 .2l48 8.8c17.4 3.2 28.9 19.9 25.7 37.2s-19.9 28.9-37.2 25.7l-47.5-8.7c-31.3-4.6-58.9-1.5-78.3 6.2s-27.2 18.3-29 28.1c-2 10.7-.5 16.7 1.2 20.4c1.8 3.9 5.5 8.3 12.8 13.2c16.3 10.7 41.3 17.7 73.7 26.3l2.9 .8c28.6 7.6 63.6 16.8 89.6 33.8c14.2 9.3 27.6 21.9 35.9 39.5c8.5 17.9 10.3 37.9 6.4 59.2c-6.9 38-33.1 63.4-65.6 76.7c-13.7 5.6-28.6 9.2-44.4 11V480c0 17.7-14.3 32-32 32s-32-14.3-32-32V445.1c-.4-.1-.9-.1-1.3-.2l-.2 0 0 0c-24.4-3.8-64.5-14.3-91.5-26.3c-16.1-7.2-23.4-26.1-16.2-42.2s26.1-23.4 42.2-16.2c20.9 9.3 55.3 18.5 75.2 21.6c31.9 4.7 58.2 2 76-5.3c16.9-6.9 24.6-16.9 26.8-28.9c1.9-10.6 .4-16.7-1.3-20.4c-1.9-4-5.6-8.4-13-13.3c-16.4-10.7-41.5-17.7-74-26.3l-2.8-.7 0 0C119.4 279.3 84.4 270 58.4 253c-14.2-9.3-27.5-22-35.8-39.6c-8.4-17.9-10.1-37.9-6.1-59.2C23.7 116 52.3 91.2 84.8 78.3c13.3-5.3 27.9-8.9 43.2-11V32c0-17.7 14.3-32 32-32z" />
                 </svg>
@@ -133,7 +133,7 @@
 
               </p>
 
-              <a href="https://www.youtube.com/watch?v=xcJtL7QggTI"
+              {{-- <a href="https://www.youtube.com/watch?v=xcJtL7QggTI"
                 data-fslightbox class="vc wf hg mb">
                 <span class="tc wf xf be dd rg i gh ua">
                   <span class="nf h vc yc vd rg gh qk -ud-z-1"></span>
@@ -141,7 +141,7 @@
                     alt="Play" />
                 </span>
                 <span class="kk">SEE HOW WE WORK</span>
-              </a>
+              </a> --}}
             </div>
           </div>
         </div>
@@ -479,7 +479,7 @@
       <!-- ===== Blog Start ===== -->
       <section class="pb-16">
         <!-- Section Title Start -->
-        <div x-data="{ sectionTitle: `Latest Blogs & News`, sectionTitleText: `Some pinned post to be displayed on website (coming soon)` }">
+        <div x-data="{ sectionTitle: `Latest Blogs & News`, sectionTitleText: `` }">
           <div class="animate_top bb ze rj ki xn vq">
             <h2 x-text="sectionTitle" class="fk vj pr kk wm on/5 gq/2 bb _b">
             </h2>
@@ -500,7 +500,7 @@
                   alt="Blog" />
 
                 <div class="im h r s df vd yc wg tc wf xf al hh/20 nl il z-10">
-                  <a href="https://www.facebook.com/107041167792621/posts/pfbid0bJ5oB74x1RZEMS4uP1MVErvpm1GJPXkaS7JYgPRt9gDeV63pUEuW7iU66JozkFaal/?mibextid=cr9u03"
+                  <a href="https://www.facebook.com/EdventureMyanmar/posts/pfbid02LR8jEs2jNQPmm11YqAzX8qjRRJKRsHFKmzYoAWaJ6q8Z6PuQN3QCcLaBZbvPkJ3al"
                     class="vc ek rg lk gh sl ml il gi hi">Read More</a>
                 </div>
               </div>
@@ -521,8 +521,8 @@
                 </div>
                 <h4 class="ek tj ml il kk wm xl eq lb">
                   <a
-                    href="https://www.facebook.com/107041167792621/posts/pfbid0bJ5oB74x1RZEMS4uP1MVErvpm1GJPXkaS7JYgPRt9gDeV63pUEuW7iU66JozkFaal/?mibextid=cr9u03">
-                    Study Engineering and technology in Thailand
+                    href="https://www.facebook.com/EdventureMyanmar/posts/pfbid02LR8jEs2jNQPmm11YqAzX8qjRRJKRsHFKmzYoAWaJ6q8Z6PuQN3QCcLaBZbvPkJ3al">
+                    Contact with us to achieve your study abroad goals
                   </a>
                 </h4>
               </div>
@@ -536,7 +536,7 @@
                   alt="Blog" />
 
                 <div class="im h r s df vd yc wg tc wf xf al hh/20 nl il z-10">
-                  <a href="https://www.facebook.com/107041167792621/posts/pfbid0288Q832VZtz4WWYbN93PMeRy36JHUpccCfadTvDLzRHeLkwCsqTU7nKA1YRS9rsdgl/?mibextid=cr9u03"
+                  <a href="https://www.facebook.com/EdventureMyanmar/posts/pfbid02LEDLPqCNNLigV679GNR6s3bdBZ3F6KTgT5Bu5XVJzX8kDocNDkhRSRAXaqaXiHutl"
                     class="vc ek rg lk gh sl ml il gi hi">Read More</a>
                 </div>
               </div>
@@ -558,8 +558,9 @@
                 </div>
                 <h4 class="ek tj ml il kk wm xl eq lb">
                   <a
-                    href="https://www.facebook.com/107041167792621/posts/pfbid0288Q832VZtz4WWYbN93PMeRy36JHUpccCfadTvDLzRHeLkwCsqTU7nKA1YRS9rsdgl/?mibextid=cr9u03">
-                    Free Academic Webinar
+                    href="https://www.facebook.com/EdventureMyanmar/posts/pfbid02LEDLPqCNNLigV679GNR6s3bdBZ3F6KTgT5Bu5XVJzX8kDocNDkhRSRAXaqaXiHutl">
+                    Special chances in our consultation
+                    session
                   </a>
                 </h4>
               </div>
@@ -573,7 +574,7 @@
                   alt="Blog" />
 
                 <div class="im h r s df vd yc wg tc wf xf al hh/20 nl il z-10">
-                  <a href="https://www.facebook.com/107041167792621/posts/pfbid079iiPq3bfbrxbUsd7oUUVsS6uvmVeU2M9kTzit7P8Hv9v1XLLuapXz5FwF1FnsC6l/?mibextid=cr9u03"
+                  <a href="https://www.facebook.com/EdventureMyanmar/posts/pfbid09oC18Gs6dpR7ycrDmQiS4UTP52GxFCE8MLsr4wAAwjC4bBSiAqvu9cQuyAKqJhUYl"
                     class="vc ek rg lk gh sl ml il gi hi">Read More</a>
                 </div>
               </div>
@@ -595,8 +596,8 @@
                 </div>
                 <h4 class="ek tj ml il kk wm xl eq lb">
                   <a
-                    href="https://www.facebook.com/107041167792621/posts/pfbid079iiPq3bfbrxbUsd7oUUVsS6uvmVeU2M9kTzit7P8Hv9v1XLLuapXz5FwF1FnsC6l/?mibextid=cr9u03">
-                    Rangsit University Scholarship Info Sharing Session
+                    href="https://www.facebook.com/EdventureMyanmar/posts/pfbid09oC18Gs6dpR7ycrDmQiS4UTP52GxFCE8MLsr4wAAwjC4bBSiAqvu9cQuyAKqJhUYl">
+                    How to book our free consultation services
                   </a>
                 </h4>
               </div>
